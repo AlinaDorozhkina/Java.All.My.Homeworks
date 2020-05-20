ead45f5d031335bd67db571fb30dd7b905b903ba
Alina_D <alinadorozhkina91@mail.ru>
2020-05-20 17:49:36 +0300

    correction of mistakes

--- src/ru/geekbrains/java/homeworks/JavaHome.java
+++ src/ru/geekbrains/java/homeworks/JavaHome.java
@@ -5,70 +5,72 @@ import java.util.Arrays;
 public class JavaHome {
 
     public static void main(String[] args) {
-        replaceArray(new int[]{1, 0, 0, 0, 0, 1, 0, 0, 0, 1});
-        fillArrayEveryThird();
-        multiPlayArray();
-        fillSquareArray();
+        System.out.println ("Задание 1:");
+        int[] array = {1, 0, 0, 0, 0, 1, 0, 0, 0, 1};
+        System.out.println(Arrays.toString(array));
+        replaceArray(array);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println ("Задание 2:");
+        int [] emptyArray = new int[8];
+        fillArrayEveryThird(emptyArray);
+        System.out.println (Arrays.toString(emptyArray));
+
+        System.out.println ("Задание 3:");
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println (Arrays.toString(arr));
+        multiPlayArray(arr);
+        System.out.println (Arrays.toString(arr));
+
+        System.out.println ("Задание 4:");
+        int [][] squareArray=new int[5][5];
+        fillSquareArray(squareArray);
+
         findMaxMinElement();
         System.out.println("Есть ли в массиве место, в котором сумма левой и правой части массива равны? " + isBalanceExist(new int []{20, 20,20,40,20}));
     }
 
     // task1
-    public static void replaceArray(int[] array) {
-        System.out.println ("Задание 1:");
-        for (int i = 0; i < array.length; i++) {
+    public static void replaceArray(int[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+           /*
             if (array[i] == 0) {
                 array[i] = 1;
             } else {
                 array[i] = 0;
             }
             System.out.print(array[i] + " ");
+
+            */
+            inputArray[i]= (inputArray[i]==0) ? 1:0;
         }
-        System.out.println();
     }
 
     //task2
-    public static void fillArrayEveryThird() {
-        System.out.println ("Задание 2: ");
-        int arrayLength = 0;
-        for (int i = 0; i <= 21; i++) {
-            if (i % 3 == 0) {
-                arrayLength++;
+    public static void fillArrayEveryThird(int [] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i]=i*3;
             }
         }
-        int[] array2 = new int[arrayLength];
-        for (int i = 0, b = 0; i <= 21; i++) {
-            if (i % 3 == 0) {
-                array2[b] = i;
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-    }
+
+
 
     // task3
-    public static void multiPlayArray() {
-        System.out.println("Задание 3: ");
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) {
-                arr[i] *= 2;
-                System.out.print(arr[i] + " ");
-            }
+    public static void multiPlayArray(int [] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i]=(inputArray[i]<6)? (inputArray[i]*2): inputArray[i];
         }
-        System.out.println();
     }
 
     //task4
-    public static void fillSquareArray() {
-        System.out.println ("Задание 4:");
-        int[][] squareArray = new int[5][5];
-        for (int i = 0; i < squareArray.length; i++) {
-            for (int j = 0; j < squareArray[i].length; j++) {
-                if (squareArray[i] == squareArray[j] || j + i == squareArray.length - 1) {
-                    squareArray[i][j] = 1;
+    public static void fillSquareArray(int[][]inputSquare) {
+
+        for (int i = 0; i < inputSquare.length; i++) {
+            for (int j = 0; j < inputSquare[i].length; j++) {
+                if (inputSquare[i] == inputSquare[j] || j + i == inputSquare.length - 1) {
+                    inputSquare[i][j] = 1;
                 }
-                System.out.print(squareArray[i][j] + " ");
+                System.out.print(inputSquare[i][j] + " ");
             }
             System.out.println();
         }
