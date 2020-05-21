13eb2cd20c421178032f63dee7e6233f96ac374c
Alina_D <alinadorozhkina91@mail.ru>
2020-05-21 17:20:03 +0300

    fixes first task

--- src/ru/geekbrains/java/homeworks/JavaHome.java
+++ src/ru/geekbrains/java/homeworks/JavaHome.java
@@ -3,32 +3,41 @@ package ru.geekbrains.java.homeworks;
 import java.util.Scanner;
 
 public class JavaHome {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Вам необходимо угадать число. У Вас есть три попытки.");
         int randomNumber = (int) (Math.random() * 9 + 1);
         guessNumber(randomNumber);
     }
-    public static void guessNumber(int inputNumber) {
-        Scanner scanner = new Scanner(System.in);
+
+    public static int getNumberFromScanner(String message, int min, int max) {
+        int userAnswer;
         do {
-            System.out.println("Угадайте число от 0 до 9. У Вас есть три попытки.");
+            System.out.println(message);
+            userAnswer = scanner.nextInt();
+        } while (userAnswer > max || userAnswer < min);
+        return userAnswer;
+    }
+
+    public static void guessNumber(int inputRandomNumber) {
+        do {
             int tryCount = 3;
             for (int i = 1; i <= 3; i++) {
-                int userNumber = scanner.nextInt();
-                if (userNumber == inputNumber) {
+                int userAnswer = getNumberFromScanner("Введите число от 0 до 9", 0, 9);
+                if (userAnswer == inputRandomNumber) {
                     System.out.println("Вы угадали! Поздравляем!");
                     break;
                 } else {
-                    System.out.println("Ваше число " + ((userNumber < inputNumber) ? " меньше" : " больше."));
+                    System.out.println("Ваше число " + ((userAnswer < inputRandomNumber) ? " меньше" : " больше."));
                     tryCount++;
                 }
             }
-            System.out.println ("Загаданное число: "+inputNumber);
+            System.out.println("Загаданное число: " + inputRandomNumber);
             System.out.println("Сыграем еще раз? Ответ: 1 - Да; Ответ: 0 - нет.");
-        } while (scanner.nextInt()==1);
+        } while (scanner.nextInt() == 1);
         scanner.close();
     }
-
 }
 
 
